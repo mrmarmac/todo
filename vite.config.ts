68080a86a4fb6c12499903c73fcd707e1fde4f12
Martin <marphill5000@gmail.com>
2026-07-06 14:54:53 +0200

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/todo/',
   plugins: [react()],
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 5173,
